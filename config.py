d86e86e78958cfeb16e538fa179a828a388ec3fa
Monalisa Dsouza <mdsouza@firstt.com>
2025-03-27 00:14:39 +0530

    Both local and prod setup done

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+
+if os.getenv("FLASK_ENV") != "production":
+    load_dotenv()
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "postgresql://flaskdb_9h9z_user:lZsAvJOh4IUABUlO6vF0wbMaUw572Pz2@dpg-cvi3vrlds78s73ej7dtg-a.oregon-postgres.render.com/flaskdb_9h9z")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
